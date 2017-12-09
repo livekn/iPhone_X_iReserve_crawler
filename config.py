57ba3752336fc446749a51b5edda99666c30a5cf
Toby@l470 <me@livekn.com>
2017-12-09 13:47:06 +0800

    tab to space

--- config.py
+++ config.py
@@ -1,48 +1,48 @@
 #coding: utf8
 class config:
-	tw = {
-		"source_url": "https://reserve-prime.apple.com/TW/zh_TW/reserve/iPhoneX/availability.json",
-		"part_name": {
-			"MQAC2TA/A": "iPhone X 64GB 太空灰色",
-			"MQAD2TA/A": "iPhone X 64GB 銀色",
-			"MQAF2TA/A": "iPhone X 256GB 太空灰色",
-			"MQAG2TA/A": "iPhone X 256GB 銀色",
-		},
-		"store_name": {
-			"R713": "台北 101",
-		},
-		"buy_url": "https://reserve-prime.apple.com/TW/zh_TW/reserve/iPhoneX/availability",
-	}
+    tw = {
+        "source_url": "https://reserve-prime.apple.com/TW/zh_TW/reserve/iPhoneX/availability.json",
+        "part_name": {
+            "MQAC2TA/A": "iPhone X 64GB 太空灰色",
+            "MQAD2TA/A": "iPhone X 64GB 銀色",
+            "MQAF2TA/A": "iPhone X 256GB 太空灰色",
+            "MQAG2TA/A": "iPhone X 256GB 銀色",
+        },
+        "store_name": {
+            "R713": "台北 101",
+        },
+        "buy_url": "https://reserve-prime.apple.com/TW/zh_TW/reserve/iPhoneX/availability",
+    }
 
-	hk = {
-		"source_url": "https://reserve-prime.apple.com/HK/zh_HK/reserve/iPhoneX/availability.json",
-		"part_name": {
-			"MQA52ZP/A": "iPhone X 64GB 太空灰色",
-			"MQA62ZP/A": "iPhone X 64GB 銀色",
-			"MQA82ZP/A": "iPhone X 256GB 太空灰色",
-			"MQA92ZP/A": "iPhone X 256GB 銀色",
-		},
-		"store_name": {
-			"R499": "Canton Road",
-			"R409": "Causeway Bay",
-			"R485": "Festival Walk",
-			"R610": "New Town Plaza",
-			"R673": "apm Hong Kong",
-			"R428": "ifc mall",
-		},
-		"buy_url": "https://reserve-prime.apple.com/HK/zh_HK/reserve/iPhoneX/availability",
-	}
+    hk = {
+        "source_url": "https://reserve-prime.apple.com/HK/zh_HK/reserve/iPhoneX/availability.json",
+        "part_name": {
+            "MQA52ZP/A": "iPhone X 64GB 太空灰色",
+            "MQA62ZP/A": "iPhone X 64GB 銀色",
+            "MQA82ZP/A": "iPhone X 256GB 太空灰色",
+            "MQA92ZP/A": "iPhone X 256GB 銀色",
+        },
+        "store_name": {
+            "R499": "Canton Road",
+            "R409": "Causeway Bay",
+            "R485": "Festival Walk",
+            "R610": "New Town Plaza",
+            "R673": "apm Hong Kong",
+            "R428": "ifc mall",
+        },
+        "buy_url": "https://reserve-prime.apple.com/HK/zh_HK/reserve/iPhoneX/availability",
+    }
 
-	mo = {
-		"source_url": "https://reserve-prime.apple.com/MO/zh_MO/reserve/iPhoneX/availability.json",
-		"part_name": {
-			"MQA52ZA/A": "iPhone X 64GB 太空灰色",
-			"MQA62ZA/A": "iPhone X 64GB 銀色",
-			"MQA82ZA/A": "iPhone X 256GB 太空灰色",
-			"MQA92ZA/A": "iPhone X 256GB 銀色",
-		},
-		"store_name": {
-			"R672": "澳門銀河",
-		},
-		"buy_url": "https://reserve-prime.apple.com/MO/zh_MO/reserve/iPhoneX/availability",
-	}
+    mo = {
+        "source_url": "https://reserve-prime.apple.com/MO/zh_MO/reserve/iPhoneX/availability.json",
+        "part_name": {
+            "MQA52ZA/A": "iPhone X 64GB 太空灰色",
+            "MQA62ZA/A": "iPhone X 64GB 銀色",
+            "MQA82ZA/A": "iPhone X 256GB 太空灰色",
+            "MQA92ZA/A": "iPhone X 256GB 銀色",
+        },
+        "store_name": {
+            "R672": "澳門銀河",
+        },
+        "buy_url": "https://reserve-prime.apple.com/MO/zh_MO/reserve/iPhoneX/availability",
+    }
--- iphone_x_crawler_mo.py
+++ iphone_x_crawler_mo.py
@@ -10,35 +10,35 @@ config = c.config()
 headers = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8"}
 
 def main():
-	# 以下以澳門版為例，如果要其他地方版，就讀 config 中不同的國家，例如 config.tw
+    # 以下以澳門版為例，如果要其他地方版，就讀 config 中不同的國家，例如 config.tw
 
-	while True:
-		response = requests.get(config.mo['source_url'], headers=headers)
+    while True:
+        response = requests.get(config.mo['source_url'], headers=headers)
 
-		# 如果 Apple 不是 return 200，30 秒後重試，常在晚上 Apple 維護網站時發生
-		while response.status_code != 200:
-			print("Apple response: {}".format(response.status_code))
-			print(response.text)
-			time.sleep(30)
+        # 如果 Apple 不是 return 200，30 秒後重試，常在晚上 Apple 維護網站時發生
+        while response.status_code != 200:
+            print("Apple response: {}".format(response.status_code))
+            print(response.text)
+            time.sleep(30)
 
-		data_mo = response.json()
-		# data_mo = {"isToday":True,"launchDate":{"zh_MO":"2017 年 11 月 23 日星期四"},"updated":1512796380119,"stores":{"R672":{"MQA82ZA/A":{"availability":{"contract":False,"unlocked":False}},"MQA52ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA92ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA62ZA/A":{"availability":{"contract":False,"unlocked":False}}}}}
+        data_mo = response.json()
+        # data_mo = {"isToday":True,"launchDate":{"zh_MO":"2017 年 11 月 23 日星期四"},"updated":1512796380119,"stores":{"R672":{"MQA82ZA/A":{"availability":{"contract":False,"unlocked":False}},"MQA52ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA92ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA62ZA/A":{"availability":{"contract":False,"unlocked":False}}}}}
 
-		available = False
+        available = False
 
-		for store, store_data in data_mo['stores'].items():
-			for part_name, part_data in store_data.items():
-				if part_data['availability']['contract'] or part_data['availability']['unlocked']:
-					available = True
-					print("{}: {} available: {}".format(datetime.datetime.now().isoformat(), config.mo['part_name'][part_name], config.mo['store_name'][store]))
-					# 也可以在這裏加上你喜歡的通知方法，我個人愛用 Telegram 去接收，再簡單點用 IFTTT 的 Webhooks 也可以
+        for store, store_data in data_mo['stores'].items():
+            for part_name, part_data in store_data.items():
+                if part_data['availability']['contract'] or part_data['availability']['unlocked']:
+                    available = True
+                    print("{}: {} available: {}".format(datetime.datetime.now().isoformat(), config.mo['part_name'][part_name], config.mo['store_name'][store]))
+                    # 也可以在這裏加上你喜歡的通知方法，我個人愛用 Telegram 去接收，再簡單點用 IFTTT 的 Webhooks 也可以
 
-		# 沒有貨
-		if not(available):
-			print("{}: Not available".format(datetime.datetime.now().isoformat()))
+        # 沒有貨
+        if not(available):
+            print("{}: Not available".format(datetime.datetime.now().isoformat()))
 
-		# 20 秒後再試
-		time.sleep(20)
+        # 20 秒後再試
+        time.sleep(20)
 
 if __name__ == "__main__":
-	main()
+    main()
