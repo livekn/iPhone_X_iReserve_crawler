0266dbfb25edfbf26fec8616bfbac91b7dd2949e
Toby@l470 <me@livekn.com>
2018-01-14 18:22:57 +0800

    Easier to modify the target country.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 pip install requests
 ```
 2. 把你國家或地區的 iPhone X 資訊加到 config.py，不過台灣、香港和澳門都已經內建，不用自己加。
-3. 修改爬蟲中的 config 所指的國家或地區，預設是澳門的 config.mo，如要改成台灣，就把全部的 config.mo 改成 config.tw。
+3. 修改 config.py 中所指定要爬的國家或地區，預設是澳門 country = "mo"，如果要爬台灣，可以改成 country = "tw"。
 
 ## 直接到網站觀看
 
--- iphone_x_crawler.py
+++ iphone_x_crawler.py
@@ -10,10 +10,11 @@ config = c.config()
 headers = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8"}
 
 def main():
-    # 以下以澳門版為例，如果要其他地方版，就讀 config 中不同的國家，例如 config.tw
+    # 如果希望轉換其他地方，可以修改 config.py 中的 country
+    country_info = config.country_info[config.country]
 
     while True:
-        response = requests.get(config.mo['source_url'], headers=headers)
+        response = requests.get(country_info['source_url'], headers=headers)
 
         # 如果 Apple 不是 return 200，30 秒後重試，常在晚上 Apple 維護網站時發生
         while response.status_code != 200:
@@ -21,16 +22,16 @@ def main():
             print(response.text)
             time.sleep(30)
 
-        data_mo = response.json()
-        # data_mo = {"isToday":True,"launchDate":{"zh_MO":"2017 年 11 月 23 日星期四"},"updated":1512796380119,"stores":{"R672":{"MQA82ZA/A":{"availability":{"contract":False,"unlocked":False}},"MQA52ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA92ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA62ZA/A":{"availability":{"contract":False,"unlocked":False}}}}}
+        data_response = response.json()
+        # data_response = {"isToday":True,"launchDate":{"zh_MO":"2017 年 11 月 23 日星期四"},"updated":1512796380119,"stores":{"R672":{"MQA82ZA/A":{"availability":{"contract":False,"unlocked":False}},"MQA52ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA92ZA/A":{"availability":{"contract":False,"unlocked":True}},"MQA62ZA/A":{"availability":{"contract":False,"unlocked":False}}}}}
 
         available = False
 
-        for store, store_data in data_mo['stores'].items():
+        for store, store_data in data_response['stores'].items():
             for part_name, part_data in store_data.items():
                 if part_data['availability']['contract'] or part_data['availability']['unlocked']:
                     available = True
-                    print("{}: {} available: {}".format(datetime.datetime.now().isoformat(), config.mo['part_name'][part_name], config.mo['store_name'][store]))
+                    print("{}: {} available: {}".format(datetime.datetime.now().isoformat(), country_info['part_name'][part_name], country_info['store_name'][store]))
                     # 也可以在這裏加上你喜歡的通知方法，我個人愛用 Telegram 去接收，再簡單點用 IFTTT 的 Webhooks 也可以
 
         # 沒有貨
